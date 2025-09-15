924daf8832119abb1a226eb231a96c46d1f9894a
tyngu123 <yagofelixaguiar@gmail.com>
2025-09-15 16:05:00 -0300

    first commit

---
+++ src/main/java/br/com/yago/socialmedia_api/SocialmediaApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.yago.socialmedia_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialmediaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialmediaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/yago/socialmedia_api/SocialmediaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.yago.socialmedia_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialmediaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
